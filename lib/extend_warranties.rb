ae738b4fca46a407836d61e04542a75f4e3314b6
Allison Reilly <acreilly3@gmail.com>
2020-04-03 12:19:36 -0300

    Init

---
+++ lib/extend_warranties.rb
@@ -0,0 +1,6 @@
+require "extend_warranties/version"
+
+module ExtendWarranties
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/extend_warranties/version.rb
@@ -0,0 +1,3 @@
+module ExtendWarranties
+  VERSION = "0.1.0"
+end
